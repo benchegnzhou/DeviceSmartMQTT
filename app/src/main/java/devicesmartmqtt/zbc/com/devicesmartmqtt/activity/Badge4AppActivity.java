322dc10d5c8bfb7dd6caa1e0f1092b3db5b2f3fb
benchengzhou <1107472286@qq.com>
2019-05-12 13:47:19 +0800

    创建提交

---
+++ app/src/main/java/devicesmartmqtt/zbc/com/devicesmartmqtt/activity/Badge4AppActivity.java
@@ -0,0 +1,15 @@
+package devicesmartmqtt.zbc.com.devicesmartmqtt.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import devicesmartmqtt.zbc.com.devicesmartmqtt.R;
+
+public class Badge4AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge4_app);
+    }
+}
---
+++ app/src/main/java/devicesmartmqtt/zbc/com/devicesmartmqtt/activity/MainActivity.java
@@ -0,0 +1,48 @@
+package devicesmartmqtt.zbc.com.devicesmartmqtt.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.jaeger.library.StatusBarUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import devicesmartmqtt.zbc.com.devicesmartmqtt.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.btn_test_badge)
+    Button btnTestBadge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setStatusbar();
+    }
+
+
+    @OnClick({R.id.btn_test_badge})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_test_badge:
+                break;
+        }
+    }
+
+
+    /**
+     * 设置状态栏
+     */
+    private void setStatusbar() {
+        //状态栏透明色
+        //StatusBarUtil.setTranslucent(this, 1);
+//        StatusBarUtil.setColorNoTranslucent(this,  getResources().getColor(R.color.comment_person) );
+//        StatusBarUtil.setLightMode(this);
+    }
+}
